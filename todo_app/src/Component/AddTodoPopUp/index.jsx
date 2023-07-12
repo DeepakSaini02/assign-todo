d96604a53fabd800394dd08a92598bdbd0fe03f7
DeepakSaini02 <ds1504484@gmail.com>
2023-07-12 08:19:25 +0530

    added local storage

--- todo_app/src/Component/AddTodoPopUp/index.jsx
+++ todo_app/src/Component/AddTodoPopUp/index.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react"
 import "./style.css"
 export const AddTodoPopUp=({handleClose})=>{
-    
+  const [task,setTask]=useState('')
+
+  const handleSubmit=()=>{
+    if(!task){
+      return;
+    }
+ console.log(task);
+  }
+
+  const handleChange=(val)=>{
+    setTask(val)
+  }
+
     return <div className="popup-box">
     <div className="box">
       <span className="close-icon" onClick={handleClose}>x</span>
-      hellow world
+      <input onChange={(event)=>handleChange(event.target.value)} type="text" value={task}/>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   </div>
 }
---
+++ todo_app/src/Service/localStorage.js
@@ -0,0 +1,15 @@
+export const addItemToLocalStorage=(key,value)=>{
+    const data=localStorage.getItem(key)
+    
+    if(!data) localStorage.setItem(key,JSON.stringify([value]))
+    else localStorage.setItem(key,JSON.stringify([...JSON.parse(data),value]))
+}
+
+export const addLocalStorage=(key,value)=>{
+    localStorage.setItem(key,JSON.stringify(value))
+}
+
+export const getItemFromLocalStorage=(key)=>{
+    const data=localStorage.getItem(key)
+    return JSON.parse(data);
+}
